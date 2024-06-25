c7435556a8924873484b11d2e149941a3835696e
it <kyle9742@naver.com>
2024-06-26 08:40:44 +0900

    first commit, add navbar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <div className='app'>
+      <nav><Navbar /></nav>
+      <main>라우팅</main>
+    </div>
+  )
+}
+
+export default App
