415b484f767d822558dfa1ab545b88496a2a3313
it <kyle9742@naver.com>
2024-06-26 10:47:32 +0900

    useForm 사용하여 로그인 페이지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import LoginPage from "./components/Authentication/LoginPage";
 import CartPage from "./components/Cart/CartPage";
 import HomePage from "./components/Home/HomePage";
 import MyOrderPage from "./components/MyOrder/MyorderPage";
@@ -17,7 +18,8 @@ function App() {
         {/* <ProductsPage /> */}
         {/* <SingleProductPage /> */}
         {/* <CartPage /> */}
-        <MyOrderPage />
+        {/* <MyOrderPage /> */}
+        <LoginPage />
       </main>
     </div>
   );
---
+++ src/components/Authentication/LoginPage.jsx
@@ -0,0 +1,30 @@
+import { useRef, useState } from "react";
+import "./LoginPage.css";
+import { useForm } from "react-hook-form";
+
+const LoginPage = () => {
+  const { register, handleSubmit } = useForm();
+
+  return (
+    <section className="align_center form_page">
+      <form className="authentication_form" onSubmit={handleSubmit((formData) => console.log(formData))}>
+        <h2>로그인 폼</h2>
+        <div className="form_inputs">
+          <div>
+            <label htmlFor="email">Email</label>
+            <input {...register("email")} type="email" id="email" className="form_text_input" placeholder="이메일 입력..." />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input {...register("password")} type="password" id="password" className="form_text_input" placeholder="패스워드 입력..." />
+          </div>
+          <button type="submit" className="search_button form_submit">
+            Submit
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default LoginPage;
