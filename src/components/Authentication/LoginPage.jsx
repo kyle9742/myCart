d090206d8e7dbc6de6224bbda2130f22ffd39cad
it <kyle9742@naver.com>
2024-06-26 11:01:11 +0900

    LoginForm 완성

--- src/components/Authentication/LoginPage.jsx
+++ src/components/Authentication/LoginPage.jsx
@@ -2,7 +2,11 @@ import "./LoginPage.css";
 import { useForm } from "react-hook-form";
 
 const LoginPage = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const submitData = (formData) => console.log(formData);
 
@@ -13,11 +17,22 @@ const LoginPage = () => {
         <div className="form_inputs">
           <div>
             <label htmlFor="email">Email</label>
-            <input {...register("email")} type="email" id="email" className="form_text_input" placeholder="이메일 입력..." />
+            <input {...register("email", { required: "이메일을 입력해주세요." })} type="email" id="email" className="form_text_input" placeholder="이메일 입력..." />
+            {errors.email && <em className="form_error">{errors.email.message}</em>}
           </div>
           <div>
             <label htmlFor="password">Password</label>
-            <input {...register("password")} type="password" id="password" className="form_text_input" placeholder="패스워드 입력..." />
+            <input
+              {...register("password", {
+                required: "패스워드를 입력해주세요.",
+                minLength: { value: 4, message: "패스워드는 최소 4자 이상." },
+              })}
+              type="password"
+              id="password"
+              className="form_text_input"
+              placeholder="패스워드 입력..."
+            />
+            {errors.password && <em className="form_error">{errors.password.message}</em>}
           </div>
           <button type="submit" className="search_button form_submit">
             Submit
