401848c761015317413907ebe5c3b6a4ae5e9d3e
it <kyle9742@naver.com>
2024-06-27 15:28:14 +0900

    jwt 암호 풀고 유저 업데이트

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
 import "./App.css";
 import Routing from "./components/Routing/Routing";
 import Navbar from "./components/Navbar/Navbar";
-
+import { useEffect, useState } from "react";
+import { jwtDecode } from "jwt-decode";
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [formError, setFormError] = useState("");
+
+  useEffect(() => {
+    try {
+      const jwt = localStorage.getItem("token");
+      const jwtUser = jwtDecode(jwt);
+      setUser(jwtUser);
+    } catch (err) {
+      setFormError(err.response.data.message);
+    }
+  }, []);
+
   return (
-    <div className='app'>
-			<Navbar />
-			<main>
-				<Routing />
-			</main>
-		</div>
+    <div className="app">
+      <Navbar />
+      <main>
+        <Routing />
+      </main>
+    </div>
   );
 }
 
