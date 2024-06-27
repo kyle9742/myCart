e28ed78abe6f88fccefcbbc2f81c35c933c462ac
it <kyle9742@naver.com>
2024-06-27 10:45:47 +0900

    페이지네이션 추가

--- src/components/Products/ProductsList.jsx
+++ src/components/Products/ProductsList.jsx
@@ -3,12 +3,21 @@ import ProductCard from "./ProductCard";
 import useData from "../../Hook/useData";
 import ProductCardSkeleton from "./ProductCardSkeleton";
 import { useSearchParams } from "react-router-dom";
+import Pagination from "../Common/Pagination";
 
 const ProductsList = () => {
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
   const [search, setSearch] = useSearchParams();
   const category = search.get("category");
-  const { data, error, isLoading } = useData("/products", { params: { category } }, [category]);
+  const page = search.get('page');
+  
+  const handlePageChange = (page) => {
+    const currentParams = Object.fromEntries([...search]);
+    setSearch({...currentParams, page: page});
+  }
+  
+  const { data, error, isLoading } = useData("/products", { params: { category, page } }, [category, page]);
+  
   
   return (
     <section className="products_list_section">
@@ -27,6 +36,13 @@ const ProductsList = () => {
         {error && <em className="form_error">{error}</em>}
         {isLoading && skeletons.map((n) => (<ProductCardSkeleton key={n} />))}
         {data.products && data.products.map((product) => (<ProductCard key={product._id} id={product._id} image={product.images[0]} price={product.price} title={product.title} rating={product.rating} ratingCounts={product.reviews.counts} stock={product.stock} />))}
+        {data && (
+					<Pagination
+						total={data.totalProducts}
+						perPage={8}
+						onClick={handlePageChange}
+						currentPage={page}
+					/>)}
       </div>
     </section>
   );
